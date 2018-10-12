3ca006b28cb6f70e87b250039f404151791b729c
Sebastian Chaves <jschaver@unal.edu.co>
2018-10-12 10:12:17 -0500

    Change urls to english

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resources :users
 
 	root "static_pages#home"
-	get '/ayuda', 					to: 'static_pages#help'
-	get '/quienes_somos', 	to: 'static_pages#about'
-	get '/contacto',	 			to: 'static_pages#contact'
+	get '/help', 					to: 'static_pages#help'
+	get '/about', 	to: 'static_pages#about'
+	get '/contact',	 			to: 'static_pages#contact'
 
   get    '/signup',  to: 'users#new'
   post   '/signup',  to: 'users#create'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
   end
 
