14832e965130cc732ce79d36e6856319b330a133
Sebastian Chaves <jschaver@unal.edu.co>
2018-10-12 08:42:45 -0500

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   resources :users
 
+	root "static_pages#home"
+	get '/ayuda', 					to: 'static_pages#help'
+	get '/quienes_somos', 	to: 'static_pages#about'
+	get '/contacto',	 			to: 'static_pages#contact'
+
   get    '/signup',  to: 'users#new'
   post   '/signup',  to: 'users#create'
 
