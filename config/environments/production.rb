686dd31afac78c494cefdaa5172766185ac0bf59
Sebastian Chaves <jschavesr@unal.edu.co>
2019-01-31 14:52:48 -0500

    Change domain name
    ups  :v

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 	# Config heroku mailer
 	config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'agropazco.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
